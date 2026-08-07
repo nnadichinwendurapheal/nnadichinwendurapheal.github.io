99ed167bc6524ff595bc11b7455392b84d34e334
nnadichinwendurapheal <nnadichinwendurapheal@gmail.com>
2026-08-07 09:19:46 +0100

    Add homepage hero slider

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
-// ===============================
-// SECUCC Health Care Pharmacy v2.0
-// ===============================
+const slides = document.querySelectorAll(".slide");
 
-// Scroll to Top Button
-const scrollBtn = document.querySelector(".scroll-top");
+let currentSlide = 0;
 
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-        scrollBtn.style.display = "flex";
-    } else {
-        scrollBtn.style.display = "none";
-    }
-});
-
-scrollBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
+function showSlide(index) {
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
     });
-});
 
-// Search Button
-const searchButton = document.querySelector(".search-box button");
-const searchInput = document.querySelector(".search-box input");
+    slides[index].classList.add("active");
+}
 
-searchButton.addEventListener("click", () => {
+setInterval(() => {
+    currentSlide++;
 
-    const medicine = searchInput.value.trim();
-
-    if (medicine === "") {
-        alert("Please enter a medicine name.");
-    } else {
-        alert("You searched for: " + medicine);
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
     }
 
-});
+    showSlide(currentSlide);
 
-// Welcome Message
-window.addEventListener("load", () => {
-    console.log("Welcome to SECUCC Health Care Pharmacy");
-});
+}, 4000);
