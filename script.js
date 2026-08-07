ce0cb1df24253a56efec08c82a8f3cb8c2fdf4b8
nnadichinwendurapheal <nnadichinwendurapheal@gmail.com>
2026-08-07 10:05:45 +0100

    Rebuild script.js

--- script.js
+++ script.js
@@ -3,20 +3,16 @@ const slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
 
 function showSlide(index) {
-    slides.forEach((slide) => {
-        slide.classList.remove("active");
-    });
+  slides.forEach(slide => {
+    slide.classList.remove("active");
+  });
 
-    slides[index].classList.add("active");
+  slides[index].classList.add("active");
 }
 
-setInterval(() => {
-    currentSlide++;
-
-    if (currentSlide >= slides.length) {
-        currentSlide = 0;
-    }
-
+if (slides.length > 0) {
+  setInterval(() => {
+    currentSlide = (currentSlide + 1) % slides.length;
     showSlide(currentSlide);
-
-}, 4000);
+  }, 4000);
+}
